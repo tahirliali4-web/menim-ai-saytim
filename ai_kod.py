b90513adae8b8974cebf56ae6a5a13f8beb8aa36
tahirliali4-web <tahirliali4@gmail.com>
2026-06-22 14:28:02 +0400

    Update ai_kod.py

--- ai_kod.py
+++ ai_kod.py
@@ -1,26 +1,27 @@
 import streamlit as st
 from sklearn.tree import DecisionTreeClassifier
 
-# 1. Başlıq əlavə edirik
 st.title("Süni İntellektlə Xəta Diaqnozu")
 
-# 2. Məlumatlar (Modeli öyrədirik)
+# 1. Məlumatlar (Modeli öyrədirik)
+# Siyahıdakı hər bir [xəta_kodu, port] cütlüyü bir nümunədir
 X_data = [[10, 80], [12, 443], [5, 21], [500, 0], [550, 0], [600, 12]]
+# 0 = Şəbəkə xətası, 1 = Sistem xətası
 y_cavablar = [0, 0, 0, 1, 1, 1] 
 
 model = DecisionTreeClassifier()
 model.fit(X_data, y_cavablar)
 
-# 3. İstifadəçidən məlumat alırıq
-xata_kodu = st.number_input("Xəta Kodu:", value=8)
-port = st.number_input("Port:", value=80)
+# 2. İstifadəçidən məlumat alırıq
+xata_kodu = st.number_input("Xəta Kodu daxil et:", value=10)
+port = st.number_input("Port nömrəsi daxil et:", value=80)
 
-# 4. Düyməyə basanda proqnoz versin
+# 3. Analiz düyməsi
 if st.button("Analiz et"):
     yeni_problem = [[xata_kodu, port]]
     texmin = model.predict(yeni_problem)
     
     if texmin[0] == 0:
-        st.error(f"Giriş {yeni_problem[0]} üçün AI Texmini: Bu bir ŞƏBƏKƏ xətasıdır!")
+        st.error(f"Nəticə: Bu bir ŞƏBƏKƏ xətasıdır!")
     else:
-        st.success(f"Giriş {yeni_problem[0]} üçün AI Texmini: Bu bir SİSTEM xətasıdır!")
+        st.success(f"Nəticə: Bu bir SİSTEM xətasıdır!")
